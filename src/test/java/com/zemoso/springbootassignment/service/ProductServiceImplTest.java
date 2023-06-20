4298f8d1010c806271e98d1025536379c678ecd1
Madhurendra Nath Tiwari <68775519+madhuredra@users.noreply.github.com>
2023-06-20 14:09:12 +0530

    removed code smell

--- src/test/java/com/zemoso/springbootassignment/service/ProductServiceImplTest.java
+++ src/test/java/com/zemoso/springbootassignment/service/ProductServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ProductServiceImplTest {
+class ProductServiceImplTest {
     @Mock
     private ProductRepository productRepository;
 
