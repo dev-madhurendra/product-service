ced847aaa3997ea6d9fcf70b265057e440df02f3
Madhurendra Nath Tiwari <68775519+madhuredra@users.noreply.github.com>
2023-06-20 15:34:49 +0530

    remove bugs from SpringBootAssignmentApplicationTests.java

--- src/test/java/com/zemoso/springbootassignment/SpringBootAssignmentApplicationTests.java
+++ src/test/java/com/zemoso/springbootassignment/SpringBootAssignmentApplicationTests.java
@@ -3,7 +3,12 @@ package com.zemoso.springbootassignment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringBootAssignmentApplicationTests {
-
+    @Test
+    void testIntegration() {
+        assertTrue(true);
+    }
 }
